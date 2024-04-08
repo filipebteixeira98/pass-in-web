2a6ac77dcbab571703a3e6e5f86516d08bac1309
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-04-07 21:58:39 -0300

    chore: add formatted date and rendered dynamically attendees information

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,7 @@
+import { ChangeEvent, useState } from 'react'
+import dayjs from 'dayjs'
+import 'dayjs/locale/en'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import {
   ChevronLeft,
   ChevronRight,
@@ -13,7 +17,19 @@ import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
 
+import { attendees } from '../data/attendees'
+
+dayjs.extend(relativeTime)
+dayjs.locale('en')
+
 export function AttendeeList() {
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -21,10 +37,12 @@ export function AttendeeList() {
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input
+            onChange={onSearchInputChanged}
             className="bg-transparent flex-1 outline-none border-0 p-0 text-sm"
             placeholder="Search attendee..."
           />
         </div>
+        {search}
       </div>
       <Table>
         <thead>
@@ -43,10 +61,10 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, index) => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
               <TableRow
-                key={index}
+                key={attendee.id}
                 className="border-b border-white/10 hover:bg-white/5"
               >
                 <TableCell>
@@ -55,17 +73,17 @@ export function AttendeeList() {
                     className="size-4 bg-black/20 rounded border border-white/10 checked:bg-orange-400"
                   />
                 </TableCell>
-                <TableCell>21786</TableCell>
+                <TableCell>{attendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Jonathan Doe
+                      {attendee.name}
                     </span>
-                    <span>jonathandoe@example.com</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 days ago</TableCell>
-                <TableCell>3 days ago</TableCell>
+                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
@@ -77,10 +95,14 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Showing 10 of 228 items</TableCell>
+            <TableCell colSpan={3}>
+              Showing 10 of {attendees.length} items
+            </TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Page 1 of 23</span>
+                <span>
+                  Page {page} of {Math.ceil(attendees.length / 10)}
+                </span>
                 <div className="flex gap-1.5">
                   <IconButton>
                     <ChevronsLeft className="size-4" />
